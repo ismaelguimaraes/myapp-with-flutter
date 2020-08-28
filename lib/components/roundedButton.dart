95247730de057f71a2e32af4aa02535da6f404fb
Ismael <Ismael@Fattorias-MacBook-Pro.local>
2020-08-27 23:01:43 -0300

    Ajustes no componente de roundedButton

--- lib/components/roundedButton.dart
+++ lib/components/roundedButton.dart
@@ -32,7 +32,11 @@ class RoundedButton extends StatelessWidget {
           onPressed: press,
           child: Text(
             text,
-            style: TextStyle(color: textColor),
+            style: TextStyle(
+              color: textColor,
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
         ),
       ),
